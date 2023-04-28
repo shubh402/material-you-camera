f06143ff1ca71190b6ee29f2195d15b8fc1cafc1
Shubh <vishwajitg471@gmail.com>
2023-04-28 18:18:42 +0530

    modify main.dart for app
    Signed-off-by: Shubh <vishwajitg471@gmail.com>

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,115 +1,170 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:math' as math;
+
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart' show join;
+import 'package:path_provider/path_provider.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  // Ensure that we have a camera available.
+  final cameras = await availableCameras();
+  if (cameras.isEmpty) {
+    print('No cameras available');
+    return;
+  }
 
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+  // Select the first available camera.
+  final camera = cameras.first;
+
+  runApp(MaterialApp(
+    title: 'Material You Camera',
+    theme: ThemeData(
+      primarySwatch: Colors.blue,
+      // Use the platform's brightness setting to determine the theme brightness.
+      brightness: Brightness.light,
+      // Use Material You colors for the app's primary color scheme.
+      colorScheme: const ColorScheme.fromSwatch(
+        primarySwatch: Colors.blueGrey,
+        brightness: Brightness.light,
+        // Use the Material You accent color for the app's secondary color scheme.
+        secondarySwatch: Colors.deepPurple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+    ),
+    home: CameraApp(camera),
+  ));
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+class CameraApp extends StatefulWidget {
+  final CameraDescription camera;
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const CameraApp(this.camera, {Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _CameraAppState createState() => _CameraAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CameraAppState extends State<CameraApp> {
+  late CameraController _controller;
+  late Future<void> _initializeControllerFuture;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+
+    // Create a CameraController for the selected camera.
+    _controller = CameraController(
+      widget.camera,
+      // Define the resolution to use.
+      ResolutionPreset.medium,
+      // Enable audio as well.
+      enableAudio: true,
+    );
+
+    // Initialize the controller. Returns a Future.
+       _initializeControllerFuture = _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    // Dispose of the controller when the widget is disposed.
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _takePicture() async {
+    try {
+      // Ensure that the camera is initialized before taking a picture.
+      await _initializeControllerFuture;
+
+      // Construct the path where the image should be saved using the
+      // `join` function from the `path` package.
+      final path = join(
+        // Store the picture in the temporary directory.
+        // Find the temp directory using the `path_provider` plugin.
+        (await getTemporaryDirectory()).path,
+        '${DateTime.now()}.png',
+      );
+
+      // Attempt to take a picture and log any errors.
+      XFile pictureFile = await _controller.takePicture();
+      File savedFile = File(pictureFile.path);
+      savedFile.renameSync(path);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void _startVideoRecording() async {
+    try {
+      // Ensure that the camera is initialized before recording.
+      await _initializeControllerFuture;
+
+      // Construct the path where the video should be saved using the
+      // `join` function from the `path` package.
+      final path = join(
+        // Store the video in the temporary directory.
+        // Find the temp directory using the `path_provider` plugin.
+        (await getTemporaryDirectory()).path,
+        '${DateTime.now()}.mp4',
+      );
+
+      // Start recording to a temporary file.
+      await _controller.startVideoRecording(path);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  void _stopVideoRecording() async {
+    try {
+      // Stop recording to a temporary file and retrieve the final path.
+      XFile videoFile = await _controller.stopVideoRecording();
+      String savedPath = videoFile.path;
+
+      // Construct the path where the video should be saved using the
+      // `join` function from the `path` package.
+      final path = join(
+        // Store the video in the temporary directory.
+        // Find the temp directory using the `path_provider` plugin.
+        (await getTemporaryDirectory()).path,
+        '${DateTime.now()}.mp4',
+      );
+
+      // Rename the video file to the final path.
+      File savedFile = File(savedPath);
+      savedFile.renameSync(path);
+    } catch (e) {
+      print(e);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Material You Camera'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+      body: FutureBuilder<void>(
+        future: _initializeControllerFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            // If the Future is complete, display the preview.
+            return CameraPreview(_controller);
+          } else {
+            // Otherwise, display a loading indicator.
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: _takePicture,
+        tooltip: 'Take a picture',
+        child: const Icon(Icons.camera_alt),
+      ),
     );
   }
 }
